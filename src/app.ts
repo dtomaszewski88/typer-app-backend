3ce38c8fb1b764d8c3834373a8753eba9faffb97
dawid <dtomaszewski88@gmail.com>
2021-05-13 19:54:26 +0200

    add gameover logic

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import logger from "morgan";
 import mongoose from "mongoose";
 import { v4 as uuid } from "uuid";
 import cors from "cors";
-import socket, { Server } from "socket.io";
+import { Server } from "socket.io";
 import type { Socket } from "socket.io";
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-const words = [
+const WORDS = [
   "weather",
   "wedding",
   "week",
@@ -33,39 +33,44 @@ const words = [
   "weekly",
   "weigh",
   "via",
-  // 'victim',
-  // 'victory',
-  // 'video',
-  // 'view',
-  // 'viewer',
-  // 'village',
-  // 'violate',
-  // 'violation',
-  // 'violence',
-  // 'violent',
-  // 'virtually',
-  // 'virtue',
-  // 'virus',
-  // 'visible',
-  // 'vision',
-  // 'visit',
-  // 'visitor',
-  // 'visual',
-  // 'vital',
+  "victim",
+  "victory",
+  "video",
+  "view",
+  "viewer",
+  "village",
+  "violate",
+  "violation",
+  "violence",
+  "violent",
+  "virtually",
+  "virtue",
+  "virus",
+  "visible",
+  "vision",
+  "visit",
+  "visitor",
+  "visual",
+  "vital",
 ];
+
 app.get("/words", function (req, res) {
-  return res.send(words);
+  return res.send(getWords(WORDS));
 });
+
+const getWords = (words: ReadonlyArray<string>) => _.slice(words, 0, 2);
 
 type Game = {
   id: string;
   words: ReadonlyArray<string>;
+  startTime: number;
   players: {
     [key: string]: {
       id: string;
       isReady: boolean;
       currentText: string;
       currentWordIndex: number;
+      score: number;
     };
   };
 };
@@ -89,10 +94,44 @@ const io = new Server(apiServer, {
   },
 });
 
-const QUEUE_CHECK_INTERVAL = 5000;
+const QUEUE_CHECK_INTERVAL = 1000;
+
+const createGame = (playerIds: string[]) => {
+  const players = _.chain(playerIds)
+    .map((playerId) => ({
+      id: playerId,
+      isReady: false,
+      currentText: "",
+      currentWordIndex: 0,
+      score: 0,
+    }))
+    .keyBy("id")
+    .value();
+  return {
+    id: uuid(),
+    words: getWords(WORDS),
+    startTime: 0,
+    players,
+  };
+};
+
+const calcScore = (word: string, time: number, errors = 0) => {
+  const timeInSec = time / 1000;
+  const minScore = word.length * 500;
+  const maxScore = Math.round((minScore * 5) / timeInSec - errors * 100);
+  console.log(minScore, maxScore, timeInSec);
+  // return minScore;
+  return Math.max(minScore, maxScore);
+};
 
 const isGameReady = (game: Game) =>
   _.chain(game).get("players").values().every(["isReady", true]).value();
+const isGameOver = (game: Game) =>
+  _.chain(game)
+    .get("players")
+    .values()
+    .some(["currentWordIndex", game.words.length])
+    .value();
 
 const setPlayerReady = (game: Game, playerId: string) => {
   game.players[playerId].isReady = true;
@@ -103,31 +142,25 @@ const setText = (game: Game, playerId: string, text: string) => {
 };
 
 const completeWord = (game: Game, playerId: string) => {
-  game.players[playerId].currentText = "";
-  game.players[playerId].currentWordIndex++;
+  const currentTime = Date.now();
+  const timeDiff = currentTime - game.startTime;
+  const playerGame = game.players[playerId];
+  const wordIndex = playerGame.currentWordIndex;
+  const currentWord = game.words[wordIndex];
+
+  playerGame.score = calcScore(currentWord, timeDiff);
+  playerGame.currentText = "";
+  playerGame.currentWordIndex++;
+};
+
+const startGame = (game: Game) => {
+  game.startTime = Date.now();
 };
 
 const checkGameQueue = () => {
   if (quickGameQueue.length >= 2) {
     const [playerId1, playerId2, ...rest] = quickGameQueue;
-    const game = {
-      id: uuid(),
-      words: words,
-      players: {
-        [playerId1]: {
-          id: playerId1,
-          isReady: false,
-          currentText: "",
-          currentWordIndex: 0,
-        },
-        [playerId2]: {
-          id: playerId2,
-          isReady: false,
-          currentText: "",
-          currentWordIndex: 0,
-        },
-      },
-    };
+    const game = createGame([playerId1, playerId2]);
     currentQuickGames[game.id] = game;
     const gameRoomId = `game-${game.id}`;
     io.in([playerId1, playerId2]).socketsJoin(gameRoomId);
@@ -140,26 +173,27 @@ const queueCheckInterval = setInterval(checkGameQueue, QUEUE_CHECK_INTERVAL);
 
 io.on("connection", function (socket: Socket) {
   console.log("Made socket connection", socket.id);
-  socket.on("incrementAll", (data) => {
-    console.log("incrementAll", data);
-    socket.broadcast.emit("incrementAllData", data);
-  });
   socket.on("updateLocalText", ({ gameId, currentText }) => {
     const game = currentQuickGames[gameId];
     const gameRoomId = `game-${gameId}`;
     setText(game, socket.id, currentText);
     socket.to(gameRoomId).emit("gameUpdate", game);
-    console.log("updateLocalText", gameId, currentText);
+    // console.log("updateLocalText", gameId, currentText);
   });
   socket.on("completeWord", ({ gameId }) => {
     const game = currentQuickGames[gameId];
     const gameRoomId = `game-${gameId}`;
     completeWord(game, socket.id);
-    socket.to(gameRoomId).emit("gameUpdate", game);
+    console.log("IS OVER", isGameOver(game));
+    if (isGameOver(game)) {
+      io.in(gameRoomId).emit("gameOver", game);
+    } else {
+      socket.to(gameRoomId).emit("gameUpdate", game);
+    }
   });
   socket.on("gameSearchInit", (data) => {
     quickGameQueue.push(socket.id);
-    console.log("gameSearchInit", data);
+    // console.log("gameSearchInit", data);
   });
   socket.on("playerReadyInit", ({ gameId }) => {
     const game = currentQuickGames[gameId];
@@ -167,6 +201,7 @@ io.on("connection", function (socket: Socket) {
     setPlayerReady(game, socket.id);
     socket.join(gameRoomId);
     if (isGameReady(game)) {
+      startGame(game);
       io.to(gameRoomId).emit("gameReadySuccess", game);
     }
   });
